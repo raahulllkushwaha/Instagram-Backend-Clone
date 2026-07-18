494a52c0f820fba3e4d6f18b389f10bbaa6112a8
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-07-18 21:49:15 +0530

    feat: Add swagger/open api

--- src/main/java/com/rahul/instagram/config/SecurityConfig.java
+++ src/main/java/com/rahul/instagram/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/*.html").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
